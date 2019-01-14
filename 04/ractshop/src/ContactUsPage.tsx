d285578dd29dc11f395434dd12fadb67a36fae98
aruyamam <aruyamam@gmail.com>
2019-01-14 20:00:49 +0900

    Creating a form with controlled components

--- 04/ractshop/src/ContactUsPage.tsx
+++ 04/ractshop/src/ContactUsPage.tsx
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import ContactUs from './ContactUs';
 
-class ContactUsPage extends Component {
+interface IState {
+   name: string;
+   email: string;
+   reason: string;
+   notes: string;
+}
+
+class ContactUsPage extends Component<{}, IState> {
+   public constructor(props: {}) {
+      super(props);
+
+      this.state = {
+         email: '',
+         name: '',
+         reason: '',
+         notes: ''
+      };
+   }
+
    public render() {
+      const { name, email, reason, notes } = this.state;
+
       return (
          <div className="page-container">
             <h1>Contact Us</h1>
@@ -9,9 +30,35 @@ class ContactUsPage extends Component {
                If you enter your details we'll get back to you as soon as we
                can.
             </p>
+            <ContactUs
+               name={name}
+               onNameChange={this.handleNameChange}
+               email={email}
+               onEmailChange={this.handleEmailChange}
+               reason={reason}
+               onReasonChange={this.handleReasonChange}
+               notes={notes}
+               onNotesChange={this.handleNotesChange}
+            />
          </div>
       );
    }
+
+   private handleNameChange = (name: string) => {
+      this.setState({ name });
+   };
+
+   private handleEmailChange = (email: string) => {
+      this.setState({ email });
+   };
+
+   private handleReasonChange = (reason: string) => {
+      this.setState({ reason });
+   };
+
+   private handleNotesChange = (notes: string) => {
+      this.setState({ notes });
+   };
 }
 
 export default ContactUsPage;
